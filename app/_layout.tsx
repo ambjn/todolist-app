12a5bc69eca018c10a1429027efa3af094ec2410
Amber Jain <jainanuamber@gmail.com>
2025-11-22 23:21:55 +0530

    feat: integrate Clerk authentication into the root layout and add Clerk and SecureStore dependencies.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
-import { Stack } from "expo-router";
+import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { Stack } from 'expo-router'
 
+const InitialLayout = () => {
+  return (
+    <Stack />
+  )
+}
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <InitialLayout />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
 }
